9fb9670f3f38663a7eef24ba6e37536d52290365
Thomas Henrique <thomas.santos02209@gmail.com>
2025-04-17 09:18:47 -0300

    Aula 3: Resend

--- app/server/mercado-pago/handle-payment.ts
+++ app/server/mercado-pago/handle-payment.ts
@@ -1,9 +1,23 @@
+import resend from "@/app/lib/resend";
 import { PaymentResponse } from "mercadopago/dist/clients/payment/commonTypes";
 
 export async function handleMercadoPagoPayment(paymentData: PaymentResponse) {
-    const metadata = paymentData.metadata
-    const userEmail = metadata.user_email
-    const testeId = metadata.teste_id
+  const metadata = paymentData.metadata;
+  const userEmail = metadata.user_email;
+  const testeId = metadata.teste_id;
 
-    console.log("PAGAMENTO COM SUCESSO: ", userEmail, testeId)
+  console.log("PAGAMENTO COM SUCESSO: ", userEmail, testeId);
+
+  const { data, error } = await resend.emails.send({
+    from: "Acme <thomas.santos063@gmail.com>",
+    to: [userEmail],
+    subject: "Pagamento realizado com sucesso",
+    text: "Pagamento realizado com sucesso!",
+  });
+
+  if (error) {
+    console.error(error);
+  }
+
+  console.log(data);
 }
