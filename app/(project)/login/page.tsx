f7ec487eeaf5054d5563c6cbea1c0531763fff68
Thomas Henrique <thomas.santos02209@gmail.com>
2025-04-17 09:02:02 -0300

    Aula 3: SEO

--- app/(project)/login/page.tsx
+++ app/(project)/login/page.tsx
@@ -1,14 +1,25 @@
 import { handleAuth } from "@/app/actions/handle-auth";
+import { auth } from "@/app/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Login() {
+export default async function Login() {
+  const session = await auth();
+
+  if (session) {
+    return redirect("/dashboard");
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-4">
       <h1 className="text-4xl font-semibold">Login</h1>
 
-      <form
-        action={handleAuth}
-      >
-        <button type="submit" className="border border-gray-400 px-4 py-2 cursor-pointer">Signin with Google</button>
+      <form action={handleAuth}>
+        <button
+          type="submit"
+          className="border border-gray-400 px-4 py-2 cursor-pointer"
+        >
+          Signin with Google
+        </button>
       </form>
     </div>
   );
